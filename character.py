c43f028660e2d20fa7226aabbc1401f931531b51
Zensify <78502455+Zensify@users.noreply.github.com>
2021-11-02 11:31:58 -0600

    Update character.py

--- character.py
+++ character.py
@@ -1,6 +1,7 @@
 import pygame
 from setup import *
 
+
 class Character(object):
     def __init__(self, x, y, w, h):
         self.x = x
@@ -34,21 +35,24 @@ class Character(object):
         elif self.x < 0:
             self.x = 0
 
-    #def bullets(self, x, y, dx, dy):
-        #self.x = y
-        #self.y = x
-        #self.dx = dy
-        #self.dy = dx
-
-        #bullet = []
-        #shoot = bullet(self.x, self.y, self.dx, self.dy)
-
-        #keystate = pygame.key.get_pressed()
-        #if keystate[pygame.K_z]
-
     def draw(self):
         pygame.draw.rect(game_window, WHITE, [self.x, self.y, self.w, self.h])
-        pygame.draw.rect(game_window, RED, [self.x, self.y, self.w, self.h])
 
     def __str__(self):
         return f"Character at [{self.x}, {self.y}]"
+
+
+class Bullet(object):
+    def __init__(self, x, y):
+        self.image = pygame.image.load("images\GreenBullet1.png")
+        self.rect = self.image.get_rect()
+        self.vel = -5
+        self.x = x
+        self.y = y
+        self.h = 20
+
+    def move(self):
+        self.y += self.vel
+
+    def draw(self):
+        game_window.blit(self.image, (self.x, self.y))
