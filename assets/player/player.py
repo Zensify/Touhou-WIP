503bc9afd60049d27b974335590853f5920c335e
Zensify <78502455+Zensify@users.noreply.github.com>
2021-11-03 11:04:06 -0600

    Add files via upload

---
+++ assets/player/player.py
@@ -0,0 +1,8 @@
+import pygame
+from character import * 
+
+class Player_Setup:
+    def __init__(self):
+        self.health = 3
+        self.damage = 5
+        self.vel = 8
---
+++ enemies.py
@@ -0,0 +1,20 @@
+import pygame, random
+from setup import *
+
+class Enemies(object):
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+    
+    def movement(self, dx, dy, vel):
+        self.dx = dx
+        self.dy = dy
+        self.vel = vel
+
+    def draw(self):
+        pygame.draw.rect(game_window, WHITE, [self.x, self.y, self.w, self.h])
+    
+    def __str__(self):
+        return f"Enemy at [{self.x}, {self.y}]"
