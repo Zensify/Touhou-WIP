0081052b7b76c52f7baf22978e2b43a10365b4d9
Zensify <78502455+Zensify@users.noreply.github.com>
2022-01-17 11:01:01 -0700

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -1,8 +1,8 @@
-from multiprocessing.connection import wait
 import pygame
 from setup import *
 from bullet import *
 from ui import *
+
 
 class Enemy_1(object):
     def __init__(self, x, y, w, h):
@@ -13,26 +13,15 @@ class Enemy_1(object):
         self.health = 1
         self.damage = 1
         self.score = 25
+        self.yspeed = 2
 
-    def move(self, velocity):
-        if self.y <= 324 and self.x <= 324:
-            self.y += 2 
-            self.x += 1
-            velocity += self.x
-
-        elif self.y >= 325 and self.x >= 325:
-            self.y -= 3
-            self.x += 4
-            velocity += self.x
-
+    def enMove(self):
+        self.x += 1
+        self.yspeed += -0.01
+        self.y += self.yspeed
 
     def draw(self):
         pygame.draw.rect(game_window, WHITE, [self.x, self.y, self.w, self.h])
 
     def __str__(self):
         return f"Enemy at [{self.x}, {self.y}]"
-
-
-# ---  Movement ---
-    # - Sine Curve
-    # - Fly down then fly Right/Left
